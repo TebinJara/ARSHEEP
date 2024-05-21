12939a63b0b882b47a3523d55ddce5dd451c6d94
Tebymoon <es.gonzalezj@duocuc.cl>
2024-05-21 15:17:46 -0400

    actualización 21-05-2024, 1
    Axios instalado

--- src/services/supa.js
+++ src/services/supa.js
@@ -1,9 +1,24 @@
 import { createClient } from '@supabase/supabase-js'
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/api/usuarios'; // Asegúrate de que la URL es correcta
+
+
 
 export const supabase = createClient(
    "https://niqxbeaxtqofvrboxnzb.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pcXhiZWF4dHFvZnZyYm94bnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4NTMyMDksImV4cCI6MjAzMDQyOTIwOX0.k025dPkt6rB55YNbs1elSUr-Zoi1CF5Of_HDOV3OENc"
 );
+
+export const obtenerUsuario = async () => {
+    try {
+        const response = await axios.get(baseUrl);
+        return response.data; // Devuelve los datos recibidos del servidor
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        return []; // Retorna un array vacío en caso de error
+    }
+};
 
 // Función obtener datos clientes
 export const obtenerClientes = async () => {
@@ -23,23 +38,6 @@ export const obtenerClientes = async () => {
     }
 };
 
-// Función obtener datos usuario
-export const obtenerUsuario = async () => {
-    try {        
-        const { data, error } = await supabase
-            .from('USUARIO')
-            .select('*'); // Seleccionar todos los campos
-
-        if (error) {
-            throw error;
-        }
-
-        return data;
-    } catch (error) {
-        console.error('Error al obtener los datos de la tabla de clientes:', error.message);
-        return null;
-    }
-};
 
 export const obtenerOrdenesDeTrabajo = async () => {
     try {
