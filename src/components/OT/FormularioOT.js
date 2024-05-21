08dc4236c58e70ea78d4a6916abef8c44a243b44
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2024-05-21 16:09:19 -0400

    actualizacion 0.21-05
    subir archivo en formulario ot

--- src/components/OT/FormularioOT.js
+++ src/components/OT/FormularioOT.js
@@ -9,12 +9,13 @@ const FormularioOT = () => {
     const [fechaVencimiento, setFechaVencimiento] = useState('');
     const [prioridad, setPrioridad] = useState('');
     const [adicional, setAdicional] = useState('');
+    const [archivo, setArchivo] = useState(null);
 
     // Función para manejar el envío del formulario
     const handleSubmit = (event) => {
         event.preventDefault();
         // Aquí puedes enviar los datos del formulario a tu backend para insertar la OT en la base de datos
-        console.log('Formulario enviado:', { descripcion, status, fechaCreacion, prioridad, adicional });
+        console.log('Formulario enviado:', { descripcion, status, fechaCreacion, prioridad, adicional, archivo });
     };
 
     return (
@@ -52,11 +53,15 @@ const FormularioOT = () => {
                         <option value="Alta">Alta</option>
                     </select>
                 </div>
-                
+                <div className="form-group">
+                    <label htmlFor="archivo">Adjuntar archivo:</label>
+                    <input type="file" className="form-control-file" id="archivo" onChange={(e) => setArchivo(e.target.files[0])} />
+                </div>
                 <div className="form-group">
                     <label htmlFor="adicional">Información Adicional:</label>
                     <textarea className="form-control" id="adicional" rows="3" placeholder="Ingrese información adicional" value={adicional} onChange={(e) => setAdicional(e.target.value)}></textarea>
                 </div>
+                
                 <button type="submit" className="btn btn-primary">Enviar OT</button>
             </form>
         </div>
