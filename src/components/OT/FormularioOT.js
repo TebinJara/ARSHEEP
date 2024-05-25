348d244b75a9f8c8fbd7503303de430f973b9315
Evelyn Pérez <106790102+evperezf@users.noreply.github.com>
2024-05-25 18:36:26 -0400

    actualizacion 0.25-05
    cambio en formulario OT, ya que presentaba duplicacion

--- src/components/OT/FormularioOT.js
+++ src/components/OT/FormularioOT.js
@@ -61,269 +61,136 @@ const FormularioOT = () => {
     };
 
     return (
-        <div className='principal-container'>
-            <div className='secondary-container'>
-                <form onSubmit={handleSubmit}>
-                    <div className='containt-header'>
-                        <h2>Información del Cliente</h2>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="descripción">Descripción:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="descripción"
-                            name="descripción"
-                            placeholder="Ingrese la descripción de la OT"
-                            value={newForm.descripción}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="status">Estado:</label>
-                        <select
-                            className="form-control"
-                            id="status"
-                            name="status"
-                            value={newForm.status}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Seleccione Estado</option>
-                            <option value="1">Ingresada</option>
-                            <option value="2">En Proceso</option>
-                            <option value="3">Cancelada</option>
-                            <option value="4">Completada</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fecha_creacion">Fecha de Creación:</label>
-                        <input
-                            type="date"
-                            className="form-control"
-                            id="fecha_creacion"
-                            name="fecha_creacion"
-                            value={newForm.fecha_creacion}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fecha_vencimiento">Fecha de Vencimiento:</label>
-                        <input
-                            type="date"
-                            className="form-control"
-                            id="fecha_vencimiento"
-                            name="fecha_vencimiento"
-                            value={newForm.fecha_vencimiento}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="prioridad">Prioridad:</label>
-                        <select
-                            className="form-control"
-                            id="prioridad"
-                            name="prioridad"
-                            value={newForm.prioridad}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Seleccione la prioridad</option>
-                            <option value="Baja">Baja</option>
-                            <option value="Media">Media</option>
-                            <option value="Alta">Alta</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="adicional">Información Adicional:</label>
-                        <textarea
-                            className="form-control"
-                            id="adicional"
-                            name="adicional"
-                            rows="3"
-                            placeholder="Ingrese información adicional"
-                            value={newForm.adicional}
-                            onChange={handleChange}
-                        ></textarea>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="run_cliente">RUT Cliente:</label>
-                        <select
-                            className="form-control"
-                            id="run_cliente"
-                            name="run_cliente"
-                            value={newForm.run_cliente}
-                            onChange={handleChange}
-                        >
-                            <option value="">Seleccione un cliente</option>
-                            {clientes.map(cliente => (
-                                <option key={cliente.run_cliente} value={cliente.run_cliente}>{cliente.nombre_cliente}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="id_empleado">Empleado:</label>
-                        <select
-                            className="form-control"
-                            id="id_empleado"
-                            name="id_empleado"
-                            value={newForm.id_empleado}
-                            onChange={handleChange}
-                        >
-                            <option value="">Seleccione un empleado</option>
-                            {empleados.map(empleado => (
-                                <option key={empleado.id_empleado} value={empleado.id_empleado}>{empleado.nombreCompleto}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="imagen_1">Imagen 1:</label>
-                        <textarea
-                            className="form-control"
-                            id="imagen_1"
-                            name="imagen_1"
-                            rows="3"
-                            placeholder="Ingrese información de la imagen"
-                            value={newForm.imagen_1}
-                            onChange={handleChange}
-                        ></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Enviar OT</button>
-                </form>
-                <form onSubmit={handleSubmit}>
-                    <div className='containt-header'>
-                        <h2>Información del Cliente</h2>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="descripción">Descripción:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="descripción"
-                            name="descripción"
-                            placeholder="Ingrese la descripción de la OT"
-                            value={newForm.descripción}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="status">Estado:</label>
-                        <select
-                            className="form-control"
-                            id="status"
-                            name="status"
-                            value={newForm.status}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Seleccione Estado</option>
-                            <option value="1">Ingresada</option>
-                            <option value="2">En Proceso</option>
-                            <option value="3">Cancelada</option>
-                            <option value="4">Completada</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fecha_creacion">Fecha de Creación:</label>
-                        <input
-                            type="date"
-                            className="form-control"
-                            id="fecha_creacion"
-                            name="fecha_creacion"
-                            value={newForm.fecha_creacion}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fecha_vencimiento">Fecha de Vencimiento:</label>
-                        <input
-                            type="date"
-                            className="form-control"
-                            id="fecha_vencimiento"
-                            name="fecha_vencimiento"
-                            value={newForm.fecha_vencimiento}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="prioridad">Prioridad:</label>
-                        <select
-                            className="form-control"
-                            id="prioridad"
-                            name="prioridad"
-                            value={newForm.prioridad}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Seleccione la prioridad</option>
-                            <option value="Baja">Baja</option>
-                            <option value="Media">Media</option>
-                            <option value="Alta">Alta</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="adicional">Información Adicional:</label>
-                        <textarea
-                            className="form-control"
-                            id="adicional"
-                            name="adicional"
-                            rows="3"
-                            placeholder="Ingrese información adicional"
-                            value={newForm.adicional}
-                            onChange={handleChange}
-                        ></textarea>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="run_cliente">RUT Cliente:</label>
-                        <select
-                            className="form-control"
-                            id="run_cliente"
-                            name="run_cliente"
-                            value={newForm.run_cliente}
-                            onChange={handleChange}
-                        >
-                            <option value="">Seleccione un cliente</option>
-                            {clientes.map(cliente => (
-                                <option key={cliente.run_cliente} value={cliente.run_cliente}>{cliente.nombre_cliente}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="id_empleado">Empleado:</label>
-                        <select
-                            className="form-control"
-                            id="id_empleado"
-                            name="id_empleado"
-                            value={newForm.id_empleado}
-                            onChange={handleChange}
-                        >
-                            <option value="">Seleccione un empleado</option>
-                            {empleados.map(empleado => (
-                                <option key={empleado.id_empleado} value={empleado.id_empleado}>{empleado.nombreCompleto}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="imagen_1">Imagen 1:</label>
-                        <textarea
-                            className="form-control"
-                            id="imagen_1"
-                            name="imagen_1"
-                            rows="3"
-                            placeholder="Ingrese información de la imagen"
-                            value={newForm.imagen_1}
-                            onChange={handleChange}
-                        ></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Enviar OT</button>
-                </form>
-            </div>
+
+
+        <div className="container">
+            <h1>Formulario de Inserción de OT</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="descripción">Descripción:</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="descripción" 
+                        name="descripción" 
+                        placeholder="Ingrese la descripción de la OT" 
+                        value={newForm.descripción} 
+                        onChange={handleChange} 
+                        required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="status">Estado:</label>
+                    <select 
+                        className="form-control" 
+                        id="status" 
+                        name="status" 
+                        value={newForm.status} 
+                        onChange={handleChange} 
+                        required>
+
+                        <option value="">Seleccione Estado</option>
+                        <option value="1">Ingresada</option>
+                        <option value="2">En Proceso</option>
+                        <option value="3">Cancelada</option>
+                        <option value="4">Completada</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="fecha_creacion">Fecha de Creación:</label>
+                    <input 
+                        type="date" 
+                        className="form-control" 
+                        id="fecha_creacion" 
+                        name="fecha_creacion" 
+                        value={newForm.fecha_creacion} 
+                        onChange={handleChange} 
+                        required 
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="fecha_vencimiento">Fecha de Vencimiento:</label>
+                    <input 
+                        type="date" 
+                        className="form-control" 
+                        id="fecha_vencimiento" 
+                        name="fecha_vencimiento" 
+                        value={newForm.fecha_vencimiento} 
+                        onChange={handleChange} 
+                        required 
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="prioridad">Prioridad:</label>
+                    <select 
+                        className="form-control" 
+                        id="prioridad" 
+                        name="prioridad" 
+                        value={newForm.prioridad} 
+                        onChange={handleChange} 
+                        required
+                    >
+                        <option value="">Seleccione la prioridad</option>
+                        <option value="Baja">Baja</option>
+                        <option value="Media">Media</option>
+                        <option value="Alta">Alta</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="adicional">Información Adicional:</label>
+                    <textarea 
+                        className="form-control" 
+                        id="adicional" 
+                        name="adicional" 
+                        rows="3" 
+                        placeholder="Ingrese información adicional" 
+                        value={newForm.adicional} 
+                        onChange={handleChange}
+                    ></textarea>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="run_cliente">RUT Cliente:</label>
+                    <select 
+                        className="form-control" 
+                        id="run_cliente" 
+                        name="run_cliente" 
+                        value={newForm.run_cliente} 
+                        onChange={handleChange}
+                    >
+                        <option value="">Seleccione un cliente</option>
+                        {clientes.map(cliente => (
+                            <option key={cliente.run_cliente} value={cliente.run_cliente}>{cliente.nombre_cliente}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="id_empleado">Empleado:</label>
+                    <select 
+                        className="form-control" 
+                        id="id_empleado" 
+                        name="id_empleado" 
+                        value={newForm.id_empleado} 
+                        onChange={handleChange}
+                    >
+                        <option value="">Seleccione un empleado</option>
+                        {empleados.map(empleado => (
+                            <option key={empleado.id_empleado} value={empleado.id_empleado}>{empleado.nombreCompleto}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="imagen_1">Imagen 1:</label>
+                    <textarea 
+                        className="form-control" 
+                        id="imagen_1" 
+                        name="imagen_1" 
+                        rows="3" 
+                        placeholder="Ingrese información de la imagen" 
+                        value={newForm.imagen_1} 
+                        onChange={handleChange}
+                    ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar OT</button>
+            </form>
         </div>
     );
 };
