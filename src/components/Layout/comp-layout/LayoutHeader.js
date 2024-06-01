9a04c116dabcd464c95b52590f4c60a94e07f954
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2024-05-31 23:00:52 -0400

    0.31-05,02
    boton que borro esteban ¬¬

--- src/components/Layout/comp-layout/LayoutHeader.js
+++ src/components/Layout/comp-layout/LayoutHeader.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "../comp-layout/LayoutHeader.css";
 
 export const LayoutHeader = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('isAuthenticated'); // Elimina el estado de autenticación del almacenamiento local
+    navigate('/'); // Redirige al usuario a la página de login
+  };
+
   return (
     <header className="header">
       <div className="logo-empresa">
@@ -11,7 +19,9 @@ export const LayoutHeader = () => {
         <label>Nombre del usuario aquí</label>
         <div className="user-image">
 
+          {/* Aquí podrías añadir la imagen del usuario si está disponible */}
         </div>
+        <button onClick={handleLogout} className="logout-button">Cerrar sesión</button>
       </div>
     </header>
   );
