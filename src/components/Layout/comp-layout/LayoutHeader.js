eedf5504e6d92149ff9f9b00ab2da2cb6b747b7e
Tebymoon <es.gonzalezj@duocuc.cl>
2024-06-01 09:18:32 -0400

    0.01-06,1

--- src/components/Layout/comp-layout/LayoutHeader.js
+++ src/components/Layout/comp-layout/LayoutHeader.js
@@ -1,14 +1,7 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 import "../comp-layout/LayoutHeader.css";
 
 export const LayoutHeader = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.removeItem('isAuthenticated'); // Elimina el estado de autenticación del almacenamiento local
-    navigate('/'); // Redirige al usuario a la página de login
-  };
 
   return (
     <header className="header">
@@ -21,7 +14,6 @@ export const LayoutHeader = () => {
 
           {/* Aquí podrías añadir la imagen del usuario si está disponible */}
         </div>
-        <button onClick={handleLogout} className="logout-button">Cerrar sesión</button>
       </div>
     </header>
   );
