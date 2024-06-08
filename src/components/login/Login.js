6946a9779b903334b5f69f65978abe0dd36c6cf1
Tebymoon <es.gonzalezj@duocuc.cl>
2024-06-08 14:06:33 -0400

    0.08-06-2024
    Cambio en el aspecto de la página de login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -33,33 +33,81 @@ export const Login = () => {
     };
 
     return (
-     
 
-            <div className='login-container'>
-                <form onSubmit={handleSubmit} className='form-container'>
-                    <div className='form-group'>
-                        <label htmlFor="username">Usuario:</label>
-                        <input
-                            id="username"
-                            type="text"
-                            value={username}
-                            onChange={e => setUsername(e.target.value)}
-                        />
+
+        <div className='login-container'>
+            <form onSubmit={handleSubmit} className='form-container'>
+                <div className='login-header'>
+                    <h1>Inicia Sesión</h1>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="username">Usuario:</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={username}
+                        placeholder='Ingresa tu correo electrónico'
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor="password">Contraseña:</label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        placeholder='Ingresa tu correo contraseña'
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Iniciar sesión</button>
+
+                <div className='login-footer'>
+                    <p>Recupera tu Contraseña</p>
+                    <p>Politicas y condiciones</p>
+                </div>
+            </form>
+            <div className='login-info'>
+
+                <div className='login-info-header'>
+                    <img src={process.env.PUBLIC_URL + '/img/isotipo-arsheep.png'} alt='Logo Arsheep' />
+                    <img src={process.env.PUBLIC_URL + '/img/logotipo-arsheep.png'} alt='Logo Arsheep' />
+                </div>
+
+                <div className='login-info-content'>
+                    <h1>Bienvenido a ARSHEEP</h1>
+                    <p>La plataforma definitiva para la gestión de trabajos de mantenimiento industrial. Aquí, los equipos de mantenimiento encuentran el lugar perfecto para planificar, ejecutar y supervisar sus tareas de manera eficiente y organizada.</p>
+                    <div className='login-info-list'>
+                            <aside>
+                                <h3>Gestión de Tareas y Proyectos</h3>
+                                <p>Crea, asigna y sigue tareas y proyectos de mantenimiento fácilmente.</p>
+                            </aside>
+                            <aside>
+                                <h3>Gestión de Tareas y Proyectos</h3>
+                                <p>Crea, asigna y sigue tareas y proyectos de mantenimiento fácilmente.</p>
+                            </aside>
+                            <aside>
+                                <h3>Gestión de Tareas y Proyectos</h3>
+                                <p>Crea, asigna y sigue tareas y proyectos de mantenimiento fácilmente.</p>
+                            </aside>
+                            <aside>
+                                <h3>Gestión de Tareas y Proyectos</h3>
+                                <p>Crea, asigna y sigue tareas y proyectos de mantenimiento fácilmente.</p>
+                            </aside>
+                            <aside>
+                                <h3>Gestión de Tareas y Proyectos</h3>
+                                <p>Crea, asigna y sigue tareas y proyectos de mantenimiento fácilmente.</p>
+                            </aside>
+
+                    
                     </div>
 
-                    <div className='form-group'>
-                        <label htmlFor="password">Contraseña:</label>
-                        <input
-                            id="password"
-                            type="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                    </div>
+                </div>
 
-                    <button type="submit">Iniciar sesión</button>
-                </form>
             </div>
+        </div>
 
     );
 
