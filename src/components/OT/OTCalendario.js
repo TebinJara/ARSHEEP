7a2fffd60f5c632d4e441bb3399edf77a1971188
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2024-06-10 11:30:53 -0400

    actualizacion 0.10-06,02
    se agrega una alerta para avisar cuando esta por vencer una OT, se mejora el calendario para queno tenga problemas con las fechas, dado a que el calendario estaba en formato gringo (domingo como inicio de semana) modificaba las alertas y la visualizacion de las ot

--- src/components/OT/OTCalendario.js
+++ src/components/OT/OTCalendario.js
@@ -5,7 +5,9 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './Calendario.css';
 import { obtenerOrdenesDeTrabajo } from '../../services/supa';
 import 'moment/locale/es';
+import Modal from '../modal/Modal';
 
+moment.locale('es'); // Configurar locale para español
 const localizer = momentLocalizer(moment);
 
 const messages = {
@@ -21,27 +23,56 @@ const messages = {
 const OTCalendario = () => {
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
+  const [alerts, setAlerts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const ordenes = await obtenerOrdenesDeTrabajo();
-        const eventsData = ordenes.map((ot) => ({
-          id: ot.id_ot,
-          title: ot.descripcion,
-          start: new Date(ot.fecha_creacion),
-          end: new Date(ot.fecha_vencimiento),
-          description: ot.diagnostico,
-          className: obtenerClasePorEstado(ot.status)
-        }));
+        const eventsData = ordenes.map((ot) => {
+          const startDate = moment(ot.fecha_creacion).startOf('day').toDate();
+          const endDate = moment(ot.fecha_vencimiento).endOf('day').toDate();
+
+          return {
+            id: ot.id_ot,
+            title: ot.descripcion,
+            start: startDate,
+            end: endDate,
+            description: ot.diagnostico,
+            status: ot.status,
+            className: obtenerClasePorEstado(ot.status)
+          };
+        });
         setEvents(eventsData);
+        checkForAlerts(eventsData);
       } catch (error) {
         console.error('Error al obtener órdenes de trabajo:', error);
       }
     };
 
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 24 * 60 * 60 * 1000); // 24 horas en milisegundos
+
+    return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
   }, []);
+
+  const checkForAlerts = (events) => {
+    const currentDate = moment().startOf('day');
+    const upcomingAlerts = events.filter(event => {
+      const endDate = moment(event.end).endOf('day');
+      const daysToEnd = endDate.diff(currentDate, 'days');
+      return (daysToEnd > 0 && daysToEnd <= 5) && (event.status === 1 || event.status === 2);
+    });
+
+    if (upcomingAlerts.length > 0) {
+      setAlerts(upcomingAlerts);
+      setShowModal(true);
+    }
+  };
 
   const handleSelectEvent = (event) => {
     setSelectedEvent(event);
@@ -76,6 +107,22 @@ const OTCalendario = () => {
         eventPropGetter={(event, start, end, isSelected) => ({
           className: event.className
         })}
+        defaultView="month"
+        views={['month', 'week', 'day', 'agenda']}
+        step={60}
+        showMultiDayTimes
+        components={{
+          week: {
+            header: ({ date, localizer }) => (
+              <div>{localizer.format(date, 'dddd DD/MM')}</div>
+            ),
+          },
+          day: {
+            header: ({ date, localizer }) => (
+              <div>{localizer.format(date, 'dddd DD/MM')}</div>
+            ),
+          },
+        }}
       />
       {selectedEvent && (
         <div className="event-details">
@@ -85,6 +132,15 @@ const OTCalendario = () => {
           <p><strong>Descripción:</strong> {selectedEvent.description}</p>
         </div>
       )}
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <h2>Alertas</h2>
+        {alerts.map(alert => (
+          <div key={alert.id} className="alert">
+            <p>La OT "{alert.title}" vencerá en {moment(alert.end).endOf('day').diff(moment().startOf('day'), 'days')} días.</p>
+          </div>
+        ))}
+        {/* <button className="modal-close" onClick={() => setShowModal(false)}>Cerrar</button> */}
+      </Modal>
     </div>
   );
 };
